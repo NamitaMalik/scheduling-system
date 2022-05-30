d757925c877179878f1fbda9fd854b4bfda4f945
NamitaMalik <namita1990@gmail.com>
2022-05-30 16:27:14 +0200

    Proxying API request to avoid CORS

--- apps/scheduler-ui/src/app/app.constant.ts
+++ apps/scheduler-ui/src/app/app.constant.ts
@@ -1,11 +1,11 @@
 import { AppConfig } from './app';
 
 export const API_URL = {
-  APPOINTMENTS: '/api/Appointments'
+  APPOINTMENTS: '/Appointments'
 }
 
 export const APP_CONFIG_PATH = '/assets/config.json'
 
 export const DEFAULT_APP_CONFIG: AppConfig = {
-  apiEndPointUrl: 'https://6oen8x7qoj.execute-api.eu-north-1.amazonaws.com/Prod'
+  apiEndPointUrl: '/api'
 }
