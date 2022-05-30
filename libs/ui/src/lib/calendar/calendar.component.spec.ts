e30dc2f0524f15b314222eb50733eba0f29fb7c4
NamitaMalik <namita1990@gmail.com>
2022-05-30 22:43:20 +0200

    Fix failing CalendarComponent spec

--- libs/ui/src/lib/calendar/calendar.component.spec.ts
+++ libs/ui/src/lib/calendar/calendar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -9,6 +10,7 @@ describe('CalendarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CalendarComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
